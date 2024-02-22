ac4d4060f1458d41d4054f9084eb84c2951ace36
Riddhesh <patilriddhesh.cse123@gmail.com>
2024-02-22 17:18:53 +0530

    done

---
+++ admin.js
@@ -0,0 +1,25 @@
+const { kafka } = require("./client");
+
+async function init() {
+  const admin = kafka.admin();
+  console.log("Admin connecting...");
+  admin.connect();
+  console.log("Admin Connection Success...");
+
+  console.log("Creating Topic [rider updates]");
+  await admin.createTopics({
+    topics: [
+      {
+        topic: "rider-updates",
+        numPartitions: 2,
+      },
+    ],
+  });
+
+  console.log("Topic Created Successfully [rider-updates]");
+
+  console.log("Disconnecting Admin...");
+  await admin.disconnect();
+}
+
+init();
